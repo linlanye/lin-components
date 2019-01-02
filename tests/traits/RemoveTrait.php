107366aec7cf25c4fbc989a5a7754c31bd763603
linlanye <linlanye@sina.cn>
2019-01-02 22:55:45 +0800

    文件路径修正

--- tests/traits/RemoveTrait.php
+++ tests/traits/RemoveTrait.php
@@ -3,7 +3,7 @@
  * @Author:             林澜叶(linlanye)
  * @Contact:            <linlanye@sina.cn>
  * @Date:               2018-07-10 11:07:13
- * @Modified time:      2019-01-02 22:40:46
+ * @Modified time:      2019-01-02 22:53:08
  * @Depends on Linker:  None
  * @Description:        对文件夹进行删除
  */
@@ -13,17 +13,18 @@ trait RemoveTrait
 {
     protected static function rmdir($dir)
     {
-        $dir = trim($dir, '/') . '/';
+        $dir = trim($dir, '/');
         if (!is_dir($dir)) {
             unlink($dir);
             return;
         }
         foreach (scandir($dir) as $file) {
             if ($file != '.' && $file != '..') {
-                if (is_dir($dir . $file)) {
-                    self::rmdir($dir . $file);
+                $file = $dir . '/' . $file;
+                if (is_dir($file)) {
+                    self::rmdir($file);
                 } else {
-                    unlink($dir . $file);
+                    unlink($file);
                 }
             }
         }
